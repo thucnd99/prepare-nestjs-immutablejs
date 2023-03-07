bb1291faf2b10104e33dd81aa0f47e050abc107f
thucnd99 <thucnd@zigvy.com>
2023-03-07 18:14:39 +0700

    resetup proj

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './auth/controllers/auth.controller';
-import { UserController } from './auth/controllers/user.controller';
-import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FeedModule } from './feed/feed.module';
 
 @Module({
-  imports: [PostsModule],
-  controllers: [AppController, AuthController, UserController],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    FeedModule,
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
