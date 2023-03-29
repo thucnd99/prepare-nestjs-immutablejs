7a9194a834434a4c193a9e006c2b8588ac97c6ad
thucnd99 <120997849+thucnd99@users.noreply.github.com>
2023-03-29 23:58:09 +0700

    better interface fieldcontrolprops

--- frontend/src/components/form.field/form.field.interface.ts
+++ frontend/src/components/form.field/form.field.interface.ts
@@ -1,9 +1,9 @@
-import React, { CSSProperties } from 'react';
+import React from 'react';
 import { CheckboxData } from './checkbox/checkbox.interface';
 import { OptionData } from './select/select.interface';
 import { RadioData } from './radio/radio.interface';
-import { FieldInputProps, FieldHelperProps } from 'formik';
-export interface FieldControlProps {
+import { FieldInputProps, FieldHelperProps, FieldAttributes } from 'formik';
+export interface FieldControlProps extends FieldAttributes<any> {
     label?: string;
     required?: boolean;
     name: string;
@@ -13,7 +13,6 @@ export interface FieldControlProps {
     dataToRender?: CheckboxData[] | CheckboxData | OptionData[] | RadioData[] | RadioData | any;
     renderComponent?: React.ReactNode;
     extra?: React.ReactNode;
-    style?: CSSProperties | undefined;
 }
 
 export interface InputControlProps extends FieldControlProps, FieldInputProps<any>, FieldHelperProps<any> {}
