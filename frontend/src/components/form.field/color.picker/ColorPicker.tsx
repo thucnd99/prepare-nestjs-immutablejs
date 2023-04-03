c122d0d7940b3bb9feac5b276f6c52bc3abb8ea7
thucnd99 <thucnd@zigvy.com>
2023-04-03 17:50:50 +0700

    fix popover color picker

--- frontend/src/components/form.field/color.picker/ColorPicker.tsx
+++ frontend/src/components/form.field/color.picker/ColorPicker.tsx
@@ -1,18 +1,14 @@
-import React, { memo, useEffect } from 'react';
+import React, { memo } from 'react';
 import { FieldControlProps } from '../form.field.interface';
 import { SketchPicker } from 'react-color';
 import CustomButton from '../../../themes/CustomButton';
-import { Button, Popover, Tooltip } from 'antd';
+import { Popover } from 'antd';
 import Input from '../Input';
 import { useField } from 'formik';
 
 const ColorPicker: React.FC<FieldControlProps> = ({
     ...props
 }) => {
-    useEffect(() => {
-        document.documentElement.scrollTop = document.documentElement.clientHeight;
-        document.documentElement.scrollLeft = document.documentElement.clientWidth;
-      }, []);
     const [field, meta, helper] = useField(props.name)
     const content = <SketchPicker color={field.value} onChange={(color, event) => {
         helper.setValue(color.hex)
@@ -22,14 +18,11 @@ const ColorPicker: React.FC<FieldControlProps> = ({
         if (visible) return;
         else helper.setTouched(true);
     }
-    return <div style={{display: 'flex',
-        flexFlow: 'row wrap'}}>
+    return <div>
         <Input style={{
-            maxWidth: `80%`,
-            minWidth: `50%`,
-            width: 'auto',
+            minWidth: '50%', width: 'auto',
             borderRadius: '10px 0px 0px 10px'}} {...props} />
-        <Popover onOpenChange={(v) => handleOpenAndTouched(v)} content={content}>
+        <Popover onOpenChange={(v) => handleOpenAndTouched(v)} content={content} trigger={'click'}>
             <CustomButton
                 style={{
                     height: '100%',
@@ -38,6 +31,7 @@ const ColorPicker: React.FC<FieldControlProps> = ({
                     width: 'fit-content',
                     borderRadius: '0px 10px 10px 0px',
                 }}
+                type="button"
                 color={field.value}>Choose color </CustomButton>
         </Popover>
     </div>
